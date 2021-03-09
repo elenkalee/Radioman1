c9f74a4aa805a41260d708f71d9fec3bdfa2ec8d
elena <elena.lipukhina@gmail.com>
2021-03-09 22:42:46 +0300

    added lombok plugin and modified Radio.java

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int minChannel = 0;
     private int maxChannel = 9;
@@ -8,38 +16,11 @@ public class Radio {
     private int currentChannel;
     private int currentVolume;
 
-    public Radio() {
-    }
 
     public Radio (int currentChannel, int maxChannel) {
         this.maxChannel = maxChannel;
         this.currentChannel = currentChannel;
     }
-
-    public Radio (int minChannel, int maxChannel, int minVolume, int maxVolume, int currentChannel, int currentVolume) {
-        this.minChannel = minChannel;
-        this.maxChannel = maxChannel;
-        this.minVolume = minVolume;
-        this.maxVolume = maxVolume;
-        this.currentChannel = currentChannel;
-        this.currentVolume = currentVolume;
-    }
-
-    public int getMinChannel() { return minChannel; }
-
-    public void setMinChannel (int minChannel) { this.minChannel = minChannel; }
-
-    public int getMaxChannel() { return maxChannel; }
-
-    public void setMaxChannel (int maxChannel) { this.maxChannel = maxChannel; }
-
-    public int getMinVolume () { return minVolume; }
-
-    public void setMinVolume (int minVolume) { this.minVolume = minVolume; }
-
-    public int getMaxVolume () { return maxVolume; }
-
-    public void setMaxVolume (int maxVolume) { this.maxVolume = maxVolume; }
 
     public int getCurrentChannel () { return currentChannel; }
 
